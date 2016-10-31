108e27631e9338fe323242dd8632ae76a933afb4
Luca Bianchi <bianchi.luca@gmail.com>
2016-10-31 17:48:42 +0100

    Completed Error object

---
+++ code/lambda/shared/ApplicationError.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var _ = require("lodash");
+
+var ApplicationError = function(code, message) {
+    code = code || ApplicationError.codes.INTERNAL_ERROR;
+    if (!_.isString(code)) {
+        throw new Error("Code parameter must be a string or undefined");
+    }
+    this.code = code;
+
+    message = message || ApplicationError.messages[code];
+
+    if ((message) && (!_.isString(message))) {
+        throw new Error("Message parameter must be a string or undefined");
+    }
+
+    this.message = message || ApplicationError.messages[code];
+
+};
+ApplicationError.codes = {
+    INTERNAL_ERROR: "INTERNAL_ERROR",
+    NOT_FOUND: "NOT_FOUND",
+    UNPROCESSABLE: "UNPROCESSABLE",
+    CONFLICT: "CONFLICT",
+    NOT_MODIFIED: "NOT_MODIFIED"
+};
+
+ApplicationError.messages = {
+    INTERNAL_ERROR: "Internal ApplicationError.",
+    NOT_FOUND: "Resource Not Found.",
+    UNPROCESSABLE: "Unable to proceed due to semantic errors.",
+    CONFLICT: "Conflict with the current state of the target resource.",
+    NOT_MODIFIED: "Resource not modified."
+};
+
+module.exports = ApplicationError;
--- spec/ErrorSpec.js
+++ spec/ErrorSpec.js
@@ -1,10 +1,10 @@
 "use strict";
 
 var CONST = {
-    MODULE_PATH: "../code/lambda/shared/Error.js"
+    MODULE_PATH: "../code/lambda/shared/ApplicationError.js"
 };
 
-describe("Error.js", function() {
+describe("ApplicationError.js", function() {
 
     it("is defined", function() {
         expect(function() {
@@ -16,17 +16,54 @@ describe("Error.js", function() {
         var Error = require(CONST.MODULE_PATH);
         describe("istantiated", function() {
 
-            it("with a code, returns an object having that code", function() {
-                var myCode = "my code";
-                var myError = new Error(myCode);
-                expect(myError).toBeDefined();
-                expect(myError.code).toBe(myCode);
+            describe("with code parameter as", function() {
+
+                it("a valid string, returns an object having that code", function() {
+                    var myCode = "my code";
+                    var myError = new Error(myCode);
+                    expect(myError).toBeDefined();
+                    expect(myError.code).toBe(myCode);
+                });
+
+                it("an empty string, sets to INTERNAL_ERROR", function() {
+                    var myError = new Error();
+                    expect(myError).toBeDefined();
+                    expect(myError.code).toBe(Error.codes.INTERNAL_ERROR);
+                });
+
+                it("anything but a string, throws an error", function() {
+                    expect(function() {
+                        new Error({});
+                    }).toThrow();
+                });
+
             });
 
-            it("with an empty string as code, sets to INTERNAL_ERROR", function() {
-                var myError = new Error();
-                expect(myError).toBeDefined();
-                expect(myError.code).toBe(Error.codes.INTERNAL_ERROR);
+            describe("with message parameter as", function() {
+
+                it("a valid string, returns an object having that message", function() {
+                    var message = "A String message";
+                    var myError = new Error("myCode", message);
+                    expect(myError.message).toBe(message);
+                });
+
+                describe("an empty string, sets ", function() {
+                    it("corresponding standard message for code", function() {
+                        var myError = new Error(Error.codes.CONFLICT);
+                        expect(myError.message).toBe(Error.messages.CONFLICT);
+                    });
+                    it("undefined if there is no standard mapping for that code", function() {
+                        var myError = new Error("myCustomErrorCode");
+                        expect(myError.message).not.toBeDefined();
+                    });
+                });
+
+                it("anything but a string, throws an error", function() {
+                    expect(function() {
+                        new Error("mycode", {});
+                    }).toThrow();
+                });
+
             });
 
         });
