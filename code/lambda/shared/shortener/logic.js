bb141aac1b2d667c51eb1ac911734bf7bf37b0c2
Luca Bianchi <bianchi.luca@gmail.com>
2016-11-02 17:49:32 +0100

    added logic and test

---
+++ code/lambda/shared/shortener/logic.js
@@ -0,0 +1,48 @@
+"use strict";
+var ApplicationError = require("../error/ApplicationError");
+
+var CONST = {
+    MODULE_NAME: "Shared/Shortener/Logic/"
+};
+
+
+var ShortenerLogic = function(logger, adapter) {
+
+    this.create = function(data) {
+        logger.verbose(CONST.MODULE_NAME + "Create");
+        return adapter.create(data)
+            .catch(function(e) {
+                if (e.code === "ConditionalCheckFailedException") {
+                    logger.warn("TrackedResource already exists");
+                    throw new ApplicationError(
+                        ApplicationError.codes.CONFLICT,
+                        "TrackedResource " + data.uuid + " already exists."
+                    );
+                }
+                throw e;
+            });
+    };
+
+    this.delete = function(id) {
+        logger.verbose(CONST.MODULE_NAME + "Delete");
+        return adapter.get(id)
+            .then(function() {
+                return adapter.delete(id);
+            });
+    };
+
+    this.list = function() {
+        logger.verbose(CONST.MODULE_NAME + "List");
+        var listPromise = adapter.list();
+        return listPromise
+            .then(function(result) {
+                return {
+                    result: result,
+                    count: listPromise.value().length
+                };
+            });
+    };
+};
+
+ShortenerLogic.$inject = ["logger", "shortener.adapter"];
+module.exports = ShortenerLogic;
---
+++ spec/shared/shortener/logicSpec.js
@@ -0,0 +1,163 @@
+"use strict";
+
+var Logic = require("../../../code/lambda/shared/shortener/logic.js"),
+    helper = require("../../helper.js"),
+    ApplicationError = require("../../../code/lambda/shared/error/ApplicationError"),
+    Promise = require("bluebird");
+
+require("jasmine-expect");
+
+describe("shared/shortener/ShortenerLogic", function() {
+    var sut, adapter;
+
+    beforeEach(function() {
+        adapter = {
+            create: function() {
+            },
+            delete: function() {
+            },
+            get: function() {
+            },
+            list: function() {
+                return Promise.resolve([]);
+            },
+            update: function() {
+            }
+        };
+        sut = new Logic(helper.getLoggerMock(), adapter);
+    });
+
+    describe("create()", function() {
+
+        it("should throw a conflict error", function(done) {
+            spyOn(adapter, "create").and.returnValue(Promise.reject({
+                message: "The conditional request failed",
+                code: "ConditionalCheckFailedException",
+                statusCode: 400,
+                retryable: false,
+                retryDelay: 0
+            }));
+            var data = {
+                uuid: "id",
+                name: "name"
+            };
+            sut.create(data)
+                .catch(function(err) {
+                    expect(adapter.create).toHaveBeenCalledWith(data);
+                    expect(err).toEqual(new ApplicationError(
+                        ApplicationError.codes.CONFLICT,
+                        "TrackedResource id already exists."
+                    ));
+                })
+                .then(done);
+        });
+
+        it("should throw a generic error", function(done) {
+            spyOn(adapter, "create").and.returnValue(Promise.reject(new Error("Generic error.")));
+            var data = {
+                uuid: "id",
+                name: "name"
+            };
+            sut.create(data)
+                .catch(function(err) {
+                    expect(adapter.create).toHaveBeenCalledWith(data);
+                    expect(err).toEqual(new Error("Generic error."));
+                })
+                .then(done);
+        });
+        it("should create an xobject", function(done) {
+            spyOn(adapter, "create").and.returnValue(Promise.resolve("dummyObject"));
+            var data = {
+                name: "name",
+                created: 1234567890
+            };
+            sut.create(data)
+                .then(function(result) {
+                    expect(adapter.create).toHaveBeenCalledWith(data);
+                    expect(result).toBe("dummyObject");
+                })
+                .then(done);
+        });
+    });
+    describe("delete()", function() {
+        it("should throw a not found error", function(done) {
+            spyOn(adapter, "get").and.returnValue(Promise.resolve(null));
+            var data = "id";
+            sut.delete(data)
+                .catch(function(err) {
+                    expect(adapter.get).toHaveBeenCalledWith("id");
+                    expect(err).toEqual(new ApplicationError(
+                        ApplicationError.codes.NOT_FOUND,
+                        "TrackedResource id not found."
+                    ));
+                })
+                .then(done);
+        });
+        it("should throw a not found error", function(done) {
+            spyOn(adapter, "get").and.returnValue(Promise.resolve({
+                uuid: "id", name: "name"
+            }));
+            var data = "id";
+            sut.delete(data)
+                .catch(function(err) {
+                    expect(adapter.get).toHaveBeenCalledWith("id");
+                    expect(err).toEqual(new ApplicationError(
+                        ApplicationError.codes.NOT_FOUND,
+                        "TrackedResource id not found."
+                    ));
+                })
+                .then(done);
+        });
+        it("should delete an xobject", function(done) {
+            spyOn(adapter, "get").and.returnValue(Promise.resolve({
+                uuid: "id",
+                name: "name",
+                owner: "owner"
+            }));
+            spyOn(adapter, "delete").and.returnValue({});
+            var data = "id";
+            sut.delete(data)
+                .then(function(result) {
+                    expect(adapter.get).toHaveBeenCalledWith("id");
+                    expect(adapter.delete).toHaveBeenCalledWith("id");
+                    expect(result).toBeEmptyObject();
+                })
+                .then(done);
+        });
+    });
+
+    describe("list()", function() {
+        beforeEach(function() {
+            spyOn(adapter, "list").and.returnValue(Promise.resolve(
+                [
+                    {
+                        uuid: "idA"
+                    },
+                    {
+                        uuid: "idB"
+                    }
+                ])
+            );
+        });
+
+        it("should return a list of resources", function(done) {
+
+            sut.list()
+                .then(function(result) {
+                    expect(adapter.list).toHaveBeenCalled();
+                    expect(result).toBeObject();
+                    expect(result.count).toBe(2);
+                    expect(result.result).toBeArrayOfSize(2);
+                    expect(result.result[0]).toEqual(
+                        {
+                            uuid: "idA"
+                        });
+                    expect(result.result[1]).toEqual(
+                        {
+                            uuid: "idB"
+                        });
+                })
+                .then(done);
+        });
+    });
+});
