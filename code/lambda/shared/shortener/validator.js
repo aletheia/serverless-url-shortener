73fee48638c1a9018c88652307a98a0ef5e43769
Luca Bianchi <bianchi.luca@gmail.com>
2016-11-02 17:22:54 +0100

    implemented validator

--- code/lambda/shared/shortener/validator.js
+++ code/lambda/shared/shortener/validator.js
@@ -1,3 +1,51 @@
-/**
- * Created by aletheia on 02/11/2016.
- */
+"use strict";
+
+var _ = require("lodash"),
+    ApplicationError = require("../error/ApplicationError"),
+    Promise = require("bluebird");
+
+var CONST = {
+    MODULE_NAME: "Shared/Shortener/Validator/"
+};
+
+var ShortenerValidator = function(logger, event, translator) {
+
+    this.create = function() {
+        logger.verbose(CONST.MODULE_NAME + "Create");
+        return Promise.try(
+            function() {
+                if (_.isNil(event.body)) {
+                    throw new ApplicationError(ApplicationError.codes.UNPROCESSABLE, "Missing event body");
+                }
+                return translator.create();
+            }
+        );
+    };
+
+    this.delete = function() {
+        logger.verbose(CONST.MODULE_NAME + "Delete");
+        return Promise.try(
+            function() {
+                if (_.isNil(event.pathParameters)) {
+                    throw new ApplicationError(ApplicationError.codes.UNPROCESSABLE, "Missing uuid in path");
+                } else if (_.isNil(event.pathParameters.uuid)) {
+                    throw new ApplicationError(ApplicationError.codes.UNPROCESSABLE, "Missing uuid in path");
+                }
+                return translator.delete();
+            }
+        );
+    };
+
+    this.list = function() {
+        logger.verbose(CONST.MODULE_NAME + "List");
+        return Promise.try(
+            function() {
+                return translator.list();
+            }
+        );
+    };
+
+
+};
+
+module.exports = ShortenerValidator;
---
+++ spec/shared/shortener/validatorSpec.js
@@ -0,0 +1,111 @@
+"use strict";
+var Validator = require("../../../code/lambda/shared/shortener/validator.js"),
+    helper = require("../../helper.js"),
+    ApplicationError = require("../../../code/lambda/shared/error/ApplicationError"),
+    Promise = require("bluebird");
+
+require("jasmine-expect");
+
+describe("shared/shortener/validator", function() {
+    var sut, translator;
+    beforeEach(function() {
+        translator = {
+            create: function() {
+            },
+            delete: function() {
+            },
+            get: function() {
+            },
+            getMetadata: function() {
+            },
+            list: function() {
+            },
+            update: function() {
+            }
+        };
+    });
+    describe("create()", function() {
+        it("should return an unprocessable error", function(done) {
+            var event = {};
+            sut = new Validator(helper.getLoggerMock(), event, translator);
+            sut.create()
+                .catch(function(err) {
+                    expect(err.code).toBe(ApplicationError.codes.UNPROCESSABLE);
+                    expect(err.message).toBe("Missing event body");
+                    expect(err).toHaveMember("stack");
+                    done();
+                });
+        });
+        it("should call the logic", function(done) {
+            var event = {
+                body: {
+                    url: "http://test.me"
+                }
+            };
+            spyOn(translator, "create").and.returnValue(Promise.resolve("dummyObject"));
+            sut = new Validator(helper.getLoggerMock(), event, translator);
+            sut.create()
+                .then(function(result) {
+                    expect(translator.create).toHaveBeenCalled();
+                    expect(result).toBe("dummyObject");
+                    done();
+                });
+        });
+    });
+
+    describe("delete()", function() {
+        it("should return an unprocessable error, if pathParameter is not defined", function(done) {
+            var event = {};
+            sut = new Validator(helper.getLoggerMock(), event, translator);
+            sut.delete()
+                .catch(function(err) {
+                    expect(err.code).toBe(ApplicationError.codes.UNPROCESSABLE);
+                    expect(err.message).toBe("Missing uuid in path");
+                    expect(err).toHaveMember("stack");
+                    done();
+                });
+        });
+        it("should return an unprocessable error, if pathParameter does not contain uuid", function(done) {
+            var event = {
+                pathParameters: {}
+            };
+            sut = new Validator(helper.getLoggerMock(), event, translator);
+            sut.delete()
+                .catch(function(err) {
+                    expect(err.code).toBe(ApplicationError.codes.UNPROCESSABLE);
+                    expect(err.message).toBe("Missing uuid in path");
+                    expect(err).toHaveMember("stack");
+                    done();
+                });
+        });
+        it("should call the logic", function(done) {
+            var event = {
+                pathParameters: {
+                    uuid: "key"
+                }
+            };
+            spyOn(translator, "delete").and.returnValue(Promise.resolve("dummyObject"));
+            sut = new Validator(helper.getLoggerMock(), event, translator);
+            sut.delete()
+                .then(function(result) {
+                    expect(translator.delete).toHaveBeenCalled();
+                    expect(result).toBe("dummyObject");
+                    done();
+                });
+        });
+    });
+
+    describe("list()", function() {
+        it("should call the logic", function(done) {
+            var event = {};
+            spyOn(translator, "list").and.returnValue(Promise.resolve("dummyObject"));
+            sut = new Validator(helper.getLoggerMock(), event, translator);
+            sut.list()
+                .then(function(result) {
+                    expect(translator.list).toHaveBeenCalled();
+                    expect(result).toBe("dummyObject");
+                    done();
+                });
+        });
+    });
+});
