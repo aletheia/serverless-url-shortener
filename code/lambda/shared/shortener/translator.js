24bef6308956c98171591000012c2cb72de49866
Luca Bianchi <bianchi.luca@gmail.com>
2016-11-02 17:23:10 +0100

    implemented translator

---
+++ code/lambda/shared/shortener/translator.js
@@ -0,0 +1,52 @@
+"use strict";
+/** @module shared/shortener/translator */
+/**
+ * @typedef {Object} TrackedResource
+ * @property {string} uuid
+ * @property {Date} created
+ * @property {Date} lastModified
+ */
+
+var _ = require("lodash");
+
+var CONST = {
+    MODULE_NAME: "Shared/Shortener/Translator/"
+};
+
+
+var ShortenerTranslator = function(logger, event, logic) {
+
+    function toDomainObject(resource) {
+        resource.created = new Date(resource.created).toISOString();
+        resource.lastModified = new Date(resource.lastModified).toISOString();
+        return resource;
+    }
+
+    this.create = function() {
+        logger.verbose(CONST.MODULE_NAME + "Create");
+        return logic.create(event.body)
+            .then(function(result) {
+                return toDomainObject(result);
+            });
+    };
+    this.delete = function() {
+        logger.verbose(CONST.MODULE_NAME + "Delete");
+        return logic.delete(event.pathParameters.uuid);
+    };
+
+    this.list = function() {
+        logger.verbose(CONST.MODULE_NAME + "List");
+        var data = {};
+        return logic.list(data)
+            .then(function(result) {
+                result.result = _.map(result.result, function(item) {
+                    return toDomainObject(item);
+                });
+                return result;
+            });
+    };
+
+};
+
+ShortenerTranslator.$inject = ["logger", "event", "shortener.logic"];
+module.exports = ShortenerTranslator;
---
+++ spec/shared/shortener/translatorSpec.js
@@ -0,0 +1,105 @@
+"use strict";
+var helper = require("../../helper.js"),
+    Promise = require("bluebird"),
+    Translator = require("../../../code/lambda/shared/shortener/translator.js");
+require("jasmine-expect");
+
+describe("shared/shortener/translator", function() {
+    var sut, logic;
+
+    beforeEach(function() {
+        logic = {
+            create: function() {
+            },
+            delete: function() {
+            },
+            get: function() {
+            },
+            getMetadata: function() {
+            },
+            list: function() {
+            },
+            update: function() {
+            }
+        };
+    });
+
+    describe("create()", function() {
+        it("should call the logic and return an object", function(done) {
+            var event = {
+                body: "dummyItem"
+            };
+            spyOn(logic, "create").and.returnValue(Promise.resolve({
+                created: 123,
+                lastModified: 456
+            }));
+            sut = new Translator(helper.getLoggerMock(), event, logic);
+            sut.create()
+                .then(function(result) {
+                    expect(logic.create).toHaveBeenCalledWith("dummyItem");
+                    expect(result.created).toBeIso8601();
+                    expect(result.lastModified).toBeIso8601();
+                    done();
+                });
+        });
+    });
+
+    describe("delete()", function() {
+        it("should call the logic", function(done) {
+            var event = {
+                pathParameters: {
+                    uuid: "key"
+                }
+            };
+            spyOn(logic, "delete").and.returnValue(Promise.resolve("dummyObject"));
+            sut = new Translator(helper.getLoggerMock(), event, logic);
+            sut.delete()
+                .then(function(result) {
+                    expect(logic.delete).toHaveBeenCalledWith("key");
+                    expect(result).toBe("dummyObject");
+                    done();
+                });
+        });
+    });
+
+    describe("list()", function() {
+        it("should call the logic", function(done) {
+            var event = {};
+            spyOn(logic, "list").and.returnValue(Promise.resolve(
+                {
+                    result: [
+                        {
+                            uuid: "id1",
+                            created: 123,
+                            lastModified: 123
+                        },
+                        {
+                            uuid: "id2",
+                            created: 456,
+                            lastModified: 456
+                        }
+                    ],
+                    count: 2
+                }));
+
+            sut = new Translator(helper.getLoggerMock(), event, logic);
+            sut.list()
+                .then(function(result) {
+                    expect(logic.list).toHaveBeenCalledWith({});
+                    expect(result.result[0]).toEqual({
+                        uuid: "id1",
+                        created: "1970-01-01T00:00:00.123Z",
+                        lastModified: "1970-01-01T00:00:00.123Z"
+                    });
+                    expect(result.result[1]).toEqual({
+                        uuid: "id2",
+                        created: "1970-01-01T00:00:00.456Z",
+                        lastModified: "1970-01-01T00:00:00.456Z"
+                    });
+                    expect(result.count).toBe(2);
+                    done();
+                });
+        });
+    });
+
+});
